38d519a9acfcbc182a3a3534a59c2eef90efefa4
Stuart Huggins <huggins@tuta.io>
2020-10-30 13:45:19 +0000

    created Document and Format models

--- WPFNotepad/Models/DocumentModel.cs
+++ WPFNotepad/Models/DocumentModel.cs
@@ -1,6 +1,28 @@
 ﻿namespace WPFNotepad.Models
 {
-  public class DocumentModel
+  public class DocumentModel : ObservableObject
   {
+    private string _text;
+    public string Text
+    {
+      get => _text;
+      set => OnPropertyChanged(ref _text, value);
+    }
+
+    private string _filePath;
+    public string FilePath
+    {
+      get => _filePath;
+      set => OnPropertyChanged(ref _filePath, value);
+    }
+
+    private string _fileName;
+    public string FileName
+    {
+      get => _fileName;
+      set => OnPropertyChanged(ref _fileName, value);
+    }
+
+    public bool IsEmpty => string.IsNullOrEmpty(FileName) || string.IsNullOrEmpty(FilePath);
   }
 }
--- WPFNotepad/Models/FormatModel.cs
+++ WPFNotepad/Models/FormatModel.cs
@@ -1,6 +1,54 @@
-﻿namespace WPFNotepad.Models
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WPFNotepad.Models
 {
-  public class FormatModel
+  public class FormatModel : ObservableObject
   {
+    private FontStyle _style;
+    public FontStyle Style
+    {
+      get => _style;
+      set => OnPropertyChanged(ref _style, value);
+    }
+
+    private FontWeight _weight;
+    public FontWeight Weight
+    {
+      get => _weight;
+      set => OnPropertyChanged(ref _weight, value);
+    }
+
+    private FontFamily _family;
+    public FontFamily Family
+    {
+      get => _family;
+      set => OnPropertyChanged(ref _family, value);
+    }
+
+    private TextWrapping _wrap;
+    public TextWrapping Wrap
+    {
+      get => _wrap;
+      set
+      {
+        OnPropertyChanged(ref _wrap, value);
+        IsWrapped = (value == TextWrapping.Wrap);
+      }
+    }
+
+    private bool _isWrapped;
+    public bool IsWrapped
+    {
+      get => _isWrapped;
+      set => OnPropertyChanged(ref _isWrapped, value);
+    }
+
+    private double _size;
+    public double Size
+    {
+      get => _size;
+      set => OnPropertyChanged(ref _size, value);
+    }
   }
 }
