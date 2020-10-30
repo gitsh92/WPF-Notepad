725188e7bc1754932132c61b0fdf88b7e57652f7
Stuart Huggins <huggins@tuta.io>
2020-10-30 18:05:10 +0000

    created main and help view models

---
+++ WPFNotepad/ViewModels/HelpViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace WPFNotepad.ViewModels
+{
+  public class HelpViewModel : ObservableObject
+  {
+    public ICommand HelpCommand { get; }
+
+    public HelpViewModel()
+    {
+      HelpCommand = new RelayCommand(null);
+    }
+
+    private void DisplayAbout()
+    {
+      // open help dialog
+    }
+  }
+}
---
+++ WPFNotepad/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using WPFNotepad.Models;
+
+namespace WPFNotepad.ViewModels
+{
+  public class MainViewModel
+  {
+    // shared active document to edit/load/save
+    private DocumentModel _document;
+
+    // manages user input for document and format styles
+    public EditorViewModel Editor { get; set; }
+
+    // manages saving and loading files
+    public FileViewModel File { get; set; }
+
+    // manage help dialog
+    public HelpViewModel Help { get; set; }
+
+    public MainViewModel()
+    {
+      _document = new DocumentModel();
+      Help = new HelpViewModel();
+      Editor = new EditorViewModel(_document);
+      File = new FileViewModel(_document);
+    }
+  }
+}
