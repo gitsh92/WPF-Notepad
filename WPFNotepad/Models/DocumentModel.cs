6e8f74d6ef9e9d0bd7a7575edd012797f0f5d708
Stuart Huggins <huggins@tuta.io>
2020-10-30 13:29:45 +0000

    created ObservableObject base class

---
+++ WPFNotepad/Models/DocumentModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WPFNotepad.Models
+{
+  public class DocumentModel
+  {
+  }
+}
---
+++ WPFNotepad/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPFNotepad
+{
+  public class ObservableObject : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged<T>(ref T property, T value, [CallerMemberName] string propertyName = "")
+    {
+      property = value;
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+}
